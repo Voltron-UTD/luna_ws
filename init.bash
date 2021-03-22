0fd71f2864ab7812ce8d397e200ad8e78f3bb495
Will Heitman <will@willheitman.com>
2021-03-21 19:13:17 -0500

    Clean up script a bit

--- init.bash
+++ init.bash
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-continue_init () {
-    # echo -n "Clearing files... "
-    # find . ! -name 'init.bash' -type rf -exec rm -r {} +
-    # echo -e "$(tput -T xterm setaf 2)OK$(tput -T xterm sgr0)"
-
-    echo -n "Sourcing ROS2... "
-    source /opt/ros/foxy/setup.bash
-    echo -e "$(tput -T xterm setaf 2)OK$(tput -T xterm sgr0)"
-
-    
-}
-
+# Trivial function, but can be extended later if needed.
 clone_repo() {
-    echo $1
     git clone $1
 }
 
+# Check for non-empty directory
 if ! (($(ls -1 | wc -l) == 2))
 then
     echo -e "[$0] $(tput -T xterm setaf 1)Error: $(tput -T xterm sgr0) Files/folders other than this script exist in the current directory. Please remove them."
@@ -25,15 +14,17 @@ fi
 
 echo -e "$(tput -T xterm setaf 6)Sourcing ROS$(tput -T xterm sgr0)"
 source /opt/ros/foxy/setup.bash
+
 echo -e "$(tput -T xterm setaf 6)Cloning repos from luna_repos.txt$(tput -T xterm sgr0)"
 mkdir src && cd src
-while read line; do clone_repo $line; done < ../luna_repos.txt
+
+# call clone_repo() on each line in txt file
+filename='../luna_repos.txt'
+while read line; do 
+    clone_repo $line
+done < $filename
+
 echo -e "$(tput -T xterm setaf 6)Installing ROS dependencies$(tput -T xterm sgr0)"
 cd ..
 rosdep install -i --from-path src --rosdistro ${ROS_DISTRO} -y
 echo -e "$(tput -T xterm setaf 2)Done.$(tput -T xterm sgr0) Luna WS is ready to be built and sourced 🎉"
-# read yn
-# case $yn in
-#     [Yy]* ) continue_init;;
-#     * ) echo "Cancelled.";;
-# esac
